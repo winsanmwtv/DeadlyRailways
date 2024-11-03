ecdca2a1d01ba38baa3b18746ca62a0ffbe33c51
winsanmwtv <wntv2020@gmail.com>
2024-11-03 22:25:06 +0700

    animation เมื่อ click ปุ่ม

--- src/ktbkonno/winsanmwtv/Launcher.java
+++ src/ktbkonno/winsanmwtv/Launcher.java
@@ -2,6 +2,8 @@ package ktbkonno.winsanmwtv;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -16,28 +18,54 @@ public class Launcher extends JPanel {
     JLabel exitButton = new JLabel();
     ImageIcon exitIcon = new ImageIcon(this.getClass().getResource("image/exit.png"));
 
+    JLabel spaces = new JLabel();
+    ImageIcon spacesIcon = new ImageIcon(this.getClass().getResource("image/spaces.png"));
+
+    ImageIcon startClick = new ImageIcon(this.getClass().getResource("image/startClick.png"));
+    ImageIcon exitClick = new ImageIcon(this.getClass().getResource("image/exitClick.png"));
+
     Launcher() {
-        setLayout(new GridLayout(3, 1));
+        setLayout(new GridLayout(4, 1));
         startButton.setIcon(startIcon);
         startTitle.setIcon(startTitleImage);
         exitButton.setIcon(exitIcon);
+        spaces.setIcon(spacesIcon);
         add(startTitle);
+        add(spaces);
         add(startButton);
         add(exitButton);
 
         startButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                // Handle the click event here
-                System.out.println("Image clicked!");
+                startButton.setIcon(startClick);
+
+                Timer timer = new Timer(25, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        startButton.setIcon(startIcon);
+                        System.out.println("tf");
+                    }
+                });
+                timer.setRepeats(false); // Ensure the timer triggers only once
+                timer.start();
             }
         });
 
         exitButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                // Handle the click event here
-                System.exit(0);
+                exitButton.setIcon(exitClick);
+
+                Timer timer = new Timer(25, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        exitButton.setIcon(exitIcon);
+                        System.exit(0);
+                    }
+                });
+                timer.setRepeats(false); // Ensure the timer triggers only once
+                timer.start();
             }
         });
     }
