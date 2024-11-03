43d1ada99bfdeaa92b42445cb380688f815ecaa2
winsanmwtv <wntv2020@gmail.com>
2024-11-03 18:07:34 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Deadly Railways
+ This minigame created by Phongsiri Loedphongthai as for Object-Oriented Programming project only
---
+++ src/ktbkonno/winsanmwtv/Main.java
@@ -0,0 +1,13 @@
+// made by Phongsiri Loedphongthai
+// ID 6604062636453 KMUTNB
+// Faculty of Applied Science, Department of Computer Science
+// You may fork this code to do anything
+
+// Deadly Railways
+package ktbkonno.winsanmwtv;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
